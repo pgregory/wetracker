97c88870b7a39f1491e73613b58dd3fc97afc092
Paul Gregory <aqsis1@gmail.com>
2016-11-16 12:54:10 +0000

    Allow setting the size of a button.
      * Use in track edit controls to make the buttons smaller.

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -8,11 +8,42 @@ import React from 'react';
 import styles from './styles.css';
 import Icon from 'react-fa';
 
+const sizes = {
+  sm: styles.sm,
+};
+
+const iconSizes = {
+  sm: styles.fa,
+};
+
 class Button extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+
+    this.classNames = this.classNames.bind(this);
+    this.iconClassNames = this.iconClassNames.bind(this);
+  }
+
+  classNames() {
+    const names = [styles.button];
+    if (this.props.size) {
+      names.push(sizes[this.props.size]);
+    }
+    return names.join(' ');
+  }
+
+  iconClassNames() {
+    const names = [];
+    if (this.props.size) {
+      names.push(iconSizes[this.props.size]);
+    }
+    return names.join(' ');
+  }
+
   render() {
     return (
-      <button className={styles.button} onClick={this.props.callBack}>
-        <Icon name={this.props.iconName} />
+      <button className={this.classNames()} onClick={this.props.callBack}>
+        <Icon className={this.iconClassNames()} name={this.props.iconName} />
       </button>
     );
   }
@@ -21,6 +52,7 @@ class Button extends React.Component { // eslint-disable-line react/prefer-state
 Button.propTypes = {
   iconName: React.PropTypes.string,
   callBack: React.PropTypes.func,
+  size: React.PropTypes.number,
 };
 
 export default Button;
