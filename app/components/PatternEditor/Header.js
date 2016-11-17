312bd482c8cbc18e3ee8847103e3020d7d9a0628
Paul Gregory <aqsis1@gmail.com>
2016-11-17 16:26:46 +0000

    Update style of track header.
     * Read name and colour from song data.
     * Add a background colour to the header, to differentiate from the
       event list.

--- app/components/PatternEditor/Header.js
+++ app/components/PatternEditor/Header.js
@@ -21,10 +21,10 @@ export default class Header extends React.Component {
       <table id="header-table">
         <thead>
           <tr>
-            { this.props.song.tracks && this.props.song.tracks.map((track, index) => (
+            { this.props.song.tracks && this.props.song.patterns[0].trackdata.map((track, index) => (
               <th key={index}>
                 <div className={styles['track-header']} style={{ width: widths[index] }}>{track.name}
-                  <div className={styles['track-color']} style={{ background: '#020' }}></div>
+                  <div className={styles['track-color']} style={{ background: track.color }}></div>
                   <div className={styles['track-controls']}>
                     <Button callBack={() => (this.onSubNoteColumn(index))} size="sm" iconName="minus" />
                     <Button callBack={() => (this.onAddNoteColumn(index))} size="sm" iconName="plus" />
