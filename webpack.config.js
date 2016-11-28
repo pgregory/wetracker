fceb5c8fb555836d14f0517f71707e1256252ff2
Paul Gregory <aqsis1@gmail.com>
2016-11-28 20:36:10 +0000

    Add webpack-dev-server and other dependencies.
     * Add url-loader, file-loader and webpack-dev-server to dependencies.
     * Add simple script target to launch the dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
     }, {
       test: /\.json$/,
       loader: 'json-loader',
