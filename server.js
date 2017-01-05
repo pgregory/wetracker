2b6bac6be46dbf32e629e37169499d09197299ba
Paul Gregory <aqsis1@gmail.com>
2017-01-05 23:51:19 +0000

    Try to get deployment to OpenShift working.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require('path');
 var serveStatic = require('serve-static')
 app = express();
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+port);
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.listen(port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + port )
+});
