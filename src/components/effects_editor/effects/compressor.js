b77093a687e9d355c5ffeee43d690d9b68360fcb
Paul Gregory <aqsis1@gmail.com>
2017-02-13 12:33:52 +0000

    Fix compressor
     * Range of release was wrong (wrong in Tuna wiki).
     * Automakeup is in parameters.

--- src/components/effects_editor/effects/compressor.js
+++ src/components/effects_editor/effects/compressor.js
@@ -21,11 +21,11 @@ class CompressorEffectUI extends EffectUIBase {
     this.bindParameterToUI("#threshold-slider", "#threshold-value", -100, 0, 0.1, "threshold");
     this.bindParameterToUI("#makeup-slider", "#makeup-value", 0, 12, 0.1, "makeupGain");
     this.bindParameterToUI("#attack-slider", "#attack-value", 0, 1000, 1, "attack");
-    this.bindParameterToUI("#release-slider", "#release-value", 0, 3000, 1, "release");
+    this.bindParameterToUI("#release-slider", "#release-value", 0, 1000, 1, "release");
     this.bindParameterToUI("#ratio-slider", "#ratio-value", 1, 20, 0.1, "ratio");
     this.bindParameterToUI("#knee-slider", "#knee-value", 0, 40, 0.1, "knee");
     this.panel.find("#automakeup").on("change", (e) => {
-      this.effect.automakeup = !e.target.checked;
+      this.effect.parameters.automakeup = e.target.checked;
       this.effectChanged(this.location, this.effect);
     });
     this.panel.find("#bypass").on("change", (e) => {
