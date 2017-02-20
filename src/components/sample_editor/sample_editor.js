1543e5aca03baabc7deaccfd204c3d88856eb262
Paul Gregory <aqsis1@gmail.com>
2017-02-20 22:35:12 +0000

    Lint sample_editor.js

--- src/components/sample_editor/sample_editor.js
+++ src/components/sample_editor/sample_editor.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 
-import Signal from '../../utils/signal';
+import { connect } from '../../utils/signal';
 import { state } from '../../state';
 import { song } from '../../utils/songmanager';
 
 import sampleTemplate from './templates/sample.marko';
 
-import styles from './styles.css';
+import './styles.css';
 
 export default class SampleEditor {
   constructor(target) {
@@ -24,21 +24,21 @@ export default class SampleEditor {
 
     this.wave_canvas = document.createElement('canvas');
 
-    this._note_names = [
-      "C-", "C#", "D-", "D#", "E-", "F-",
-      "F#", "G-", "G#", "A-", "A#", "B-"
+    this.noteName = [
+      'C-', 'C#', 'D-', 'D#', 'E-', 'F-',
+      'F#', 'G-', 'G#', 'A-', 'A#', 'B-',
     ];
 
-    Signal.connect(state, "cursorChanged", this, "onCursorChanged");
-    Signal.connect(state, "playingInstrumentsChanged", this, "onPlayingInstrumentsChanged");
-    Signal.connect(song, "songChanged", this, "onSongChanged");
-    Signal.connect(song, "instrumentChanged", this, "onInstrumentChanged");
-    Signal.connect(song, "sampleChanged", this, "onSampleChanged");
+    connect(state, 'cursorChanged', this, 'onCursorChanged');
+    connect(state, 'playingInstrumentsChanged', this, 'onPlayingInstrumentsChanged');
+    connect(song, 'songChanged', this, 'onSongChanged');
+    connect(song, 'instrumentChanged', this, 'onInstrumentChanged');
+    connect(song, 'sampleChanged', this, 'onSampleChanged');
   }
 
   updateSample() {
-    this.instrumentIndex = state.cursor.get("instrument");
-    this.sampleIndex = state.cursor.get("sample");
+    this.instrumentIndex = state.cursor.get('instrument');
+    this.sampleIndex = state.cursor.get('sample');
 
     try {
       this.instrument = song.getInstrument(this.instrumentIndex);
@@ -48,12 +48,12 @@ export default class SampleEditor {
       if (len > 0) {
         this.zoom = this.canvas.width / len;
         this.minzoom = this.zoom;
-        this.yoff = Math.pow((this.zoom * 100.0), 1/3)*100.0;
+        this.yoff = ((this.zoom * 100.0) ** (1 / 3)) * 100.0;
         this.minyoff = this.yoff;
         const maxoffset = (this.sample.len * this.zoom) - this.canvas.width;
         this.offset = Math.max(Math.min(this.offset, maxoffset), 0);
       }
-    } catch(e) {
+    } catch (e) {
       this.instrument = undefined;
       this.sample = undefined;
     }
@@ -63,30 +63,30 @@ export default class SampleEditor {
     this.wave_canvas.height = $('.sample-editor .waveform').height();
     this.wave_canvas.width = $('.sample-editor .waveform').width();
 
-    var width = this.wave_canvas.width;
-    var height = this.wave_canvas.height;
-    var ctx = this.wave_canvas.getContext('2d');
+    const width = this.wave_canvas.width;
+    const height = this.wave_canvas.height;
+    const ctx = this.wave_canvas.getContext('2d');
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, width, height);
 
     if (this.sample) {
-      var len = this.sample.len;
-      var samples = this.sample.sampledata.data;
+      const len = this.sample.len;
+      const samples = this.sample.sampledata.data;
 
-      if(this.zoom == null) {
+      if (this.zoom == null) {
         // Set default zoom to fill the window.
         this.zoom = this.canvas.width / len;
         this.minzoom = this.zoom;
-        this.yoff = Math.pow((this.zoom * 100.0), 1/3)*100.0;
+        this.yoff = ((this.zoom * 100.0) ** (1 / 3)) * 100.0;
         this.minyoff = this.yoff;
       }
 
       const pixelsPerSample = this.zoom;
       const samplesPerPixel = Math.max(1.0, Math.floor(1.0 / this.zoom));
-      // Calculate the sample window in the waveform so we only draw what is visible. 
+      // Calculate the sample window in the waveform so we only draw what is visible.
       // Take into account zoom and offset.
       let sampleWindowMin = Math.floor(this.offset / this.zoom);
-      // Now adjust the window to the start of the nearest sample "bin" so that at the same zoom level a pixel 
+      // Now adjust the window to the start of the nearest sample 'bin' so that at the same zoom level a pixel
       // shows the same sample as it shifts with the offset, this avoids jittering in the draw as the sample used
       // for each pixel shifts with the horizontal offset.
       sampleWindowMin = Math.floor(sampleWindowMin / samplesPerPixel) * samplesPerPixel;
@@ -95,19 +95,19 @@ export default class SampleEditor {
 
       ctx.strokeStyle = '#55acff';
       ctx.beginPath();
-      ctx.moveTo(0, height/2);
-      for (var i = sampleWindowMin; i < sampleWindowMax; i += sampleStep) {
-        var x = (i - sampleWindowMin) * pixelsPerSample;
-        var y = ((samples[i]*height/2) + height/2);
+      ctx.moveTo(0, height / 2);
+      for (let i = sampleWindowMin; i < sampleWindowMax; i += sampleStep) {
+        const x = (i - sampleWindowMin) * pixelsPerSample;
+        const y = (((samples[i] * height) / 2) + (height / 2));
         ctx.lineTo(x, y);
       }
       ctx.stroke();
-      if ((this.sample.type & 0x3) !== 0) {
+      if ((this.sample.type & 0x3) !== 0) { // eslint-disable-line no-bitwise
         this.loopStartMarker = (this.sample.loop - sampleWindowMin) * pixelsPerSample;
         this.loopEndMarker = (((this.sample.loop - sampleWindowMin) + this.sample.looplen) * pixelsPerSample);
 
-        ctx.strokeStyle = "#30fc05";
-        ctx.fillStyle = "#30fc05";
+        ctx.strokeStyle = '#30fc05';
+        ctx.fillStyle = '#30fc05';
         ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.moveTo(this.loopStartMarker, 0);
@@ -135,12 +135,11 @@ export default class SampleEditor {
   }
 
   drawPositions() {
-    if(this.positions && this.sample) {
+    if (this.positions && this.sample) {
       const ctx = this.canvas.getContext('2d');
-      const len = this.sample.len;
-      ctx.strokeStyle = "#F00";
+      ctx.strokeStyle = '#F00';
       ctx.lineWidth = 2;
-      for(let i = 0; i < this.positions.length; i += 1) {
+      for (let i = 0; i < this.positions.length; i += 1) {
         const displayPosition = (this.positions[i] * this.zoom) - this.offset;
         ctx.beginPath();
         ctx.moveTo(displayPosition, 0);
@@ -162,54 +161,54 @@ export default class SampleEditor {
   }
 
   updateControlPanel() {
-    if(this.sample) {
-      $(this.target).find("input#basenote").val(this.noteName(this.sample.note));
-      $(this.target).find("input#finetune").val(this.sample.fine);
-      $(this.target).find("select#loop").val(this.sample.type);
+    if (this.sample) {
+      $(this.target).find('input#basenote').val(this.noteName(this.sample.note));
+      $(this.target).find('input#finetune').val(this.sample.fine);
+      $(this.target).find('select#loop').val(this.sample.type);
     } else {
-      $(this.target).find("input#basenote").val("---");
-      $(this.target).find("input#basenote").val("0");
-      $(this.target).find("select#loop").val("0");
+      $(this.target).find('input#basenote').val('---');
+      $(this.target).find('input#basenote').val('0');
+      $(this.target).find('select#loop').val('0');
     }
   }
 
   noteName(note) {
-    return this._note_names[(note + 48) % 12] + ~~((note + 48) / 12);
+    return this.noteName[(note + 48) % 12] + ~~((note + 48) / 12); // eslint-disable-line no-bitwise
   }
 
   render() {
     const target = $(this.target);
-    target.append(sampleTemplate.renderToString({sample: this.sample}));
+    target.append(sampleTemplate.renderToString({ sample: this.sample }));
 
     this.canvas = $(this.target).find('.sample-editor .waveform canvas')[0];
 
-    target.find("button#note-down").click((e) => {
+    target.find('button#note-down').click(() => {
       this.sample.note = Math.max(-48, this.sample.note - 1);
       this.updateControlPanel();
     });
-    target.find("button#note-up").click((e) => {
+    target.find('button#note-up').click(() => {
       this.sample.note = Math.min(96, this.sample.note + 1);
       this.updateControlPanel();
     });
-    target.find("button#octave-down").click((e) => {
+    target.find('button#octave-down').click(() => {
       this.sample.note = Math.max(-48, this.sample.note - 12);
       this.updateControlPanel();
     });
-    target.find("button#octave-up").click((e) => {
+    target.find('button#octave-up').click(() => {
       this.sample.note = Math.min(96, this.sample.note + 12);
       this.updateControlPanel();
     });
 
-    target.find("button#fine-down").click((e) => {
+    target.find('button#fine-down').click(() => {
       this.sample.fine = Math.max(-128, this.sample.fine - 1);
       this.updateControlPanel();
     });
-    target.find("button#fine-up").click((e) => {
+    target.find('button#fine-up').click(() => {
       this.sample.fine = Math.min(128, this.sample.fine + 1);
       this.updateControlPanel();
     });
 
-    target.find("select#loop").change((e) => {
+    target.find('select#loop').change((e) => {
       const type = $(e.target).val();
       this.sample.type = type;
       if (type !== 0 && this.sample.loop === 0 && this.sample.looplen === 0) {
@@ -219,16 +218,16 @@ export default class SampleEditor {
       song.updateInstrument(this.instrumentIndex, this.instrument);
     });
 
-    $(this.canvas).on("mousedown", this.onMouseDown.bind(this));
-    $(this.canvas).on("mouseup", (e) => {
+    $(this.canvas).on('mousedown', this.onMouseDown.bind(this));
+    $(this.canvas).on('mouseup', () => {
       if (this.dragging) {
         song.updateInstrument(this.instrumentIndex, this.instrument);
       }
       this.dragging = false;
     });
-    $(this.canvas).on("mousemove", this.onMouseMove.bind(this));
+    $(this.canvas).on('mousemove', this.onMouseMove.bind(this));
 
-    $(this.canvas).on("mousewheel", this.onScroll.bind(this));
+    $(this.canvas).on('mousewheel', this.onScroll.bind(this));
 
     this.canvas.height = $('.sample-editor .waveform').height();
     this.canvas.width = $('.sample-editor .waveform').width();
@@ -261,11 +260,9 @@ export default class SampleEditor {
           // Dragging the loop start marker
           this.sample.looplen -= (newX - this.sample.loop);
           this.sample.loop = newX;
-          //song.updateInstrument(this.instrumentIndex, this.instrument);
-        } else if(this.dragMarker === 1) {
+        } else if (this.dragMarker === 1) {
           // Dragging the loop end marker
           this.sample.looplen = newX - this.sample.loop;
-          //song.updateInstrument(this.instrumentIndex, this.instrument);
         } else {
           // Dragging the waveform
           this.offset -= (e.offsetX - this.mouseX);
@@ -280,12 +277,10 @@ export default class SampleEditor {
   }
 
   onScroll(e) {
-    const prevOffset = this.offset;
     if (Math.abs(e.originalEvent.deltaY) > Math.abs(e.originalEvent.deltaX)) {
       this.yoff += e.originalEvent.deltaY;
       this.yoff = Math.min(Math.max(this.yoff, this.minyoff), 1000);
-      const k = 4;
-      this.zoom = Math.max(this.minzoom, Math.pow((this.yoff/100.0), 3) / 100.0);
+      this.zoom = Math.max(this.minzoom, ((this.yoff / 100.0) ** 3) / 100.0);
     } else {
       this.offset += e.originalEvent.deltaX;
     }
@@ -307,14 +302,14 @@ export default class SampleEditor {
   }
 
   onCursorChanged() {
-    if ((state.cursor.get("instrument") !== this.lastCursor.get("instrument")) ||
-        (state.cursor.get("sample") !== this.lastCursor.get("sample"))) {
+    if ((state.cursor.get('instrument') !== this.lastCursor.get('instrument')) ||
+        (state.cursor.get('sample') !== this.lastCursor.get('sample'))) {
       this.updateSample();
 
       try {
         this.updateDisplay();
         this.updateControlPanel();
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
 
@@ -329,14 +324,14 @@ export default class SampleEditor {
   }
 
   onInstrumentChanged(index) {
-    if((this.instrumentIndex != null) && (index == this.instrumentIndex)) {
+    if ((this.instrumentIndex != null) && (index === this.instrumentIndex)) {
       this.updateDisplay();
       this.updateControlPanel();
     }
   }
 
   onSampleChanged(instrumentIndex, sampleIndex) {
-    if(((this.instrumentIndex != null) && (instrumentIndex == this.instrumentIndex)) &&
+    if (((this.instrumentIndex != null) && (instrumentIndex === this.instrumentIndex)) &&
        ((this.sampleIndex != null) && (sampleIndex === this.sampleIndex))) {
       this.updateSample();
       this.updateDisplay();
@@ -345,12 +340,12 @@ export default class SampleEditor {
   }
 
   onPlayingInstrumentsChanged() {
-    const playing = state.playingInstruments.get("positions");
+    const playing = state.playingInstruments.get('positions');
     this.positions = [];
-    if(playing.has(this.instrumentIndex)) {
+    if (playing.has(this.instrumentIndex)) {
       const positions = playing.get(this.instrumentIndex);
-      positions.forEach((p, i) => {
-        this.positions.push(p.get("position"));
+      positions.forEach((p) => {
+        this.positions.push(p.get('position'));
       });
     }
     this.updateDisplayPositions();
