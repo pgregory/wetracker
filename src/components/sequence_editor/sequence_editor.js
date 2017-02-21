c287af2ee6075dc9612f16cbb2cb4553ef43dad2
Paul Gregory <aqsis1@gmail.com>
2017-02-21 21:41:56 +0000

    Lint sequence_editor.js

--- src/components/sequence_editor/sequence_editor.js
+++ src/components/sequence_editor/sequence_editor.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 
-import Signal from '../../utils/signal';
+import { connect } from '../../utils/signal';
 import { state } from '../../state';
 import { song } from '../../utils/songmanager';
 
 import sequencesTemplate from './templates/sequences.marko';
 
-import styles from './styles.css';
+import './styles.css';
 
 export default class SequenceEditor {
   constructor(target) {
@@ -14,54 +14,54 @@ export default class SequenceEditor {
     this.yoff = 0;
     this.lastCursor = undefined;
 
-    Signal.connect(state, "cursorChanged", this, "onCursorChanged");
-    Signal.connect(song, "sequenceChanged", this, "onSequenceChanged");
-    Signal.connect(song, "songChanged", this, "onSongChanged");
-    Signal.connect(song, "sequenceItemChanged", this, "onSequenceItemChanged");
+    connect(state, 'cursorChanged', this, 'onCursorChanged');
+    connect(song, 'sequenceChanged', this, 'onSequenceChanged');
+    connect(song, 'songChanged', this, 'onSongChanged');
+    connect(song, 'sequenceItemChanged', this, 'onSequenceItemChanged');
   }
 
   render() {
     const target = $(this.target);
 
     const sequence = song.getSequencePatterns();
-    target.append(sequencesTemplate.renderToString({sequence, cursor: state.cursor.toJS()}));
+    target.append(sequencesTemplate.renderToString({ sequence, cursor: state.cursor.toJS() }));
 
     try {
-      this.rowHeight = $(this.target).find(".sequence-row")[0].clientHeight;
-    } catch(e) {
+      this.rowHeight = $(this.target).find('.sequence-row')[0].clientHeight;
+    } catch (e) {
       this.rowHeight = 0;
     }
 
-    target.find(".sequence-top-padding div").height(
-      (target.height()-this.rowHeight)/2.0);
+    target.find('.sequence-top-padding div').height(
+      (target.height() - this.rowHeight) / 2.0);
 
-    target.find(".sequence-bottom-padding div").height(
-      (target.height()-this.rowHeight)/2.0);
+    target.find('.sequence-bottom-padding div').height(
+      (target.height() - this.rowHeight) / 2.0);
 
     target.find('.sequence').on('mousewheel', this.onScroll.bind(this));
 
-    target.find('button#add-pattern').click((e) => {
-      song.addPattern(state.cursor.get("sequence"));
+    target.find('button#add-pattern').click(() => {
+      song.addPattern(state.cursor.get('sequence'));
     });
 
-    target.find('button#delete-pattern').click((e) => {
-      song.deletePattern(state.cursor.get("sequence"));
+    target.find('button#delete-pattern').click(() => {
+      song.deletePattern(state.cursor.get('sequence'));
     });
 
-    target.find('button#clone-pattern').click((e) => {
-      song.clonePattern(state.cursor.get("sequence"));
+    target.find('button#clone-pattern').click(() => {
+      song.clonePattern(state.cursor.get('sequence'));
     });
 
-    target.find('button#duplicate-pattern').click((e) => {
-      song.duplicatePattern(state.cursor.get("sequence"));
+    target.find('button#duplicate-pattern').click(() => {
+      song.duplicatePattern(state.cursor.get('sequence'));
     });
 
-    target.find('button#pattern-up').click((e) => {
-      song.updateSequencePattern(state.cursor.get("sequence"), 1);
+    target.find('button#pattern-up').click(() => {
+      song.updateSequencePattern(state.cursor.get('sequence'), 1);
     });
 
-    target.find('button#pattern-down').click((e) => {
-      song.updateSequencePattern(state.cursor.get("sequence"), -1);
+    target.find('button#pattern-down').click(() => {
+      song.updateSequencePattern(state.cursor.get('sequence'), -1);
     });
 
     this.lastCursor = state.cursor.toJS();
@@ -74,9 +74,9 @@ export default class SequenceEditor {
   }
 
   showCurrentSequence() {
-    $(this.target).find(".current-pattern").removeClass('current-pattern');
-    $(this.target).find(".sequence-row").eq(state.cursor.get("sequence")).addClass('current-pattern');
-    $(this.target).find(".list-container").scrollTop(state.cursor.get("sequence")*this.rowHeight);
+    $(this.target).find('.current-pattern').removeClass('current-pattern');
+    $(this.target).find('.sequence-row').eq(state.cursor.get('sequence')).addClass('current-pattern');
+    $(this.target).find('.list-container').scrollTop(state.cursor.get('sequence') * this.rowHeight);
   }
 
   onSongChanged() {
@@ -88,12 +88,12 @@ export default class SequenceEditor {
   }
 
   onSequenceItemChanged(sequence) {
-    const s = $(this.target).find(`.sequence-row[data-sequenceindex="${sequence}"] .sequence-pattern div`);
+    const s = $(this.target).find(`.sequence-row[data-sequenceindex='${sequence}'] .sequence-pattern div`);
     s.text(song.getSequencePatternNumber(sequence));
   }
 
   onCursorChanged() {
-    if (state.cursor.get("sequence") != this.lastCursor.sequence) {
+    if (state.cursor.get('sequence') !== this.lastCursor.sequence) {
       this.showCurrentSequence();
     }
     this.lastCursor = state.cursor.toJS();
@@ -101,17 +101,17 @@ export default class SequenceEditor {
 
   onScroll(e) {
     this.yoff += e.originalEvent.deltaY;
-    if(Math.abs(this.yoff) >= this.rowHeight) {
+    if (Math.abs(this.yoff) >= this.rowHeight) {
       const rowIncr = Math.floor(this.yoff / this.rowHeight);
-      let row = state.cursor.get("sequence") + rowIncr;
+      let row = state.cursor.get('sequence') + rowIncr;
       const maxrow = song.getSequenceLength() - 1;
       row = Math.min(Math.max(row, 0), maxrow);
 
-      if(row !== this.lastCursor.sequence) {
-        var pattern = song.getSequencePatternNumber(row);
+      if (row !== this.lastCursor.sequence) {
+        const pattern = song.getSequencePatternNumber(row);
 
-        let patrow = state.cursor.get("row");
-        var maxpatrow = song.getPatternRowCount(pattern);
+        let patrow = state.cursor.get('row');
+        const maxpatrow = song.getPatternRowCount(pattern);
         patrow = ((patrow % maxpatrow) + maxpatrow) % maxpatrow;
 
         state.set({
@@ -119,7 +119,7 @@ export default class SequenceEditor {
             row: patrow,
             sequence: row,
             pattern,
-          }
+          },
         });
       }
       this.yoff -= (rowIncr * this.rowHeight);
