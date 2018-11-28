4a2b5e04256081b0e37a44b5e56857ac4783256b
Paul Gregory <aqsis1@gmail.com>
2018-11-28 14:45:52 +0000

    Update songmanager.js

--- src/utils/songmanager.js
+++ src/utils/songmanager.js
@@ -491,7 +491,7 @@ export class SongManager {
         }
       };
       xmReq.onerror = (e) => {
-        reject(`Network error fetching file: ${uri} ${xmReq.statusText}`);
+        reject(`Network error fetching file: ${uri} ${e}`);
       };
       xmReq.send(null);
     });
